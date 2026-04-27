9f61809d81863d7a704f0c9c2822c7d746cf6f1c
emaksprod <emaksprimeprod@gmail.com>
2026-04-27 17:20:39 +0300

    Run known workflow datasource seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PanelMetadataSeeder::class,
             PanelDataSourcesSeeder::class,
+            PanelKnownWorkflowDataSourcesSeeder::class,
         ]);
     }
 }
