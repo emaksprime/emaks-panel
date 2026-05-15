86450340daea87382e4175061d413f902474f574
unknown <Administrator@emaks.com.tr>
2026-05-15 16:08:31 +0300

    Add accounting finance area

---
+++ lang/tr/validation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'min' => [
+        'string' => 'Şifre en az :min karakter olmalıdır.',
+    ],
+
+    'password' => [
+        'mixed' => 'Şifre en az bir büyük harf ve bir küçük harf içermelidir.',
+    ],
+];
